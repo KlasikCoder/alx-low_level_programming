e114b9c9c9a90aad6862557bc729cc93789f6a16
KlasikCoder <klasik.multiconcept0@gmail.com>
2022-09-21 20:43:11 -0700

    Print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * Description: Prints all base 16
+ * with lower case letters
+ * starting from 0, followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+	for (ch = '0'; ch<= '9'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch ='a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
