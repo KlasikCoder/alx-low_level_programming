01626c5b5de13ceab8841eb95652a71c1926545f
KlasikCoder <klasik.multiconcept0@gmail.com>
2022-09-25 09:29:24 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
 
 int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet(void);
+void print_alphabet_x10(void);
 
 #endif
